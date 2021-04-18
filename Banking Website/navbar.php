168985e167b1b94e9c244d6c622027f5f8fe5e5c
sahi999 <56267123+sahi999@users.noreply.github.com>
2021-04-18 15:18:16 +0530

    Add files via upload

---
+++ Banking Website/navbar.php
@@ -0,0 +1,22 @@
+<!-- navbar --> 
+      <nav class="navbar navbar-expand-md navbar-light bg-light">
+      <a class="navbar-brand" href="index.php">TSF Bank</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <h5><a class="nav-link" href="index.php">Home</a></h5>
+              </li>
+              <li class="nav-item">
+                <h5><a class="nav-link" href="createuser.php">Create User</a></h5>
+              </li>
+              <li class="nav-item">
+                <h5><a class="nav-link" href="transfermoney.php">Transfer Money</a></h5>
+              </li>
+              <li class="nav-item">
+                <h5><a class="nav-link" href="transactionhistory.php">Transaction History</a></h5>
+              </li>
+          </div>
+       </nav>
